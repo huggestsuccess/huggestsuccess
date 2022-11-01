908d63e6b6c444e042da7e9c10138b40c7359d42
huggestsuccess <n_pecora@me.com>
2022-11-01 10:42:48 +0100

    Create winget.ps1

---
+++ scripts/winget.ps1
@@ -0,0 +1,22 @@
+winget source remove msstore
+winget install 7zip
+winget install powershell
+winget install starship
+winget install ccleaner
+winget install Inkscape
+winget install VivaldiTechnologies.Vivaldi
+
+winget install Microsoft.DotNet.SDK.6
+winget install vscode
+winget install GitHub.cli
+winget install GitHub.GitHubDesktop
+winget install Microsoft.VisualStudio.2022.Community
+winget install marktext
+
+winget install Valve.Steam
+winget install RiotGames.LeagueOfLegends.EUW
+winget install Discord
+winget install VideoLAN.VLC
+winget install Corsair.iCUE.4
+winget install Spotify
+winget install IObit.DriverBooster9
